920090c12d5744db54f94c7e792bed2894e18adb
Jessie Puls <jessiepuls@gmail.com>
2021-02-23 11:19:44 -0600

    Adding vnet and route table into vnet module

--- azure/vnet/main.tf
+++ azure/vnet/main.tf
@@ -6,3 +6,38 @@ resource "azurerm_resource_group" "resource_group" {
   name     = "${var.vnet_name}-resource-group"
   location = var.location
 }
+
+module "vnet" {
+  source              = "Azure/vnet/azurerm"
+  resource_group_name = azurerm_resource_group.resource_group.name
+  address_space       = var.address_space
+  subnet_prefixes     = values(var.subnets)
+  subnet_names        = keys(var.subnets)
+
+  # route_tables_ids = {
+  #   subnet1 = azurerm_route_table.example.id
+  #   subnet2 = azurerm_route_table.example.id
+  #   subnet3 = azurerm_roiute_table.example.id
+  # }
+
+  tags = var.tags
+}
+
+resource "azurerm_route_table" "route_table" {
+  name                = "${var.vnet_name}-route-table"
+  resource_group_name = azurerm_resource_group.resource_group.name
+  location            = azurerm_resource_group.resource_group.location
+}
+
+# resource "azurerm_subnet_route_table_association" "example" {
+#   subnet_id      = azurerm_subnet.example.id
+#   route_table_id = azurerm_route_table.example.id
+# }
+
+# resource "azurerm_route" "route" {
+#   name                = "acceptanceTestRoute1"
+#   resource_group_name = azurerm_resource_group.example.name
+#   route_table_name    = azurerm_route_table.example.name
+#   address_prefix      = "10.1.0.0/16"
+#   next_hop_type       = "vnetlocal"
+# }
--- azure/vnet/variables.tf
+++ azure/vnet/variables.tf
@@ -7,3 +7,29 @@ variable "vnet_name" {
   type = string
   default = "coop-vnet"
 }
+
+variable "environment" {
+  type = string
+  default = "dev"
+}
+
+variable "tags" {
+  type = map
+  default = {
+    "environment" = "dev"
+  }
+}
+
+variable "address_space" {
+  type = list
+  default = ["10.0.0.0/16"]
+}
+
+variable "subnets" {
+  type = map
+  default = {
+    "subnet1" = "10.0.1.0/24",
+    "subnet2" = "10.0.2.0/24",
+    "subnet3" = "10.0.3.0/24",
+  }
+}
