b78fb75887b0639b487244dfe5462e18bb4a6614
Jessie Puls <jessiepuls@gmail.com>
2021-02-23 17:58:22 -0600

    Making resource names generic

--- azure/vnet/main.tf
+++ azure/vnet/main.tf
@@ -7,18 +7,18 @@ resource "azurerm_resource_group" "resource_group" {
   location = var.location
 }
 
-resource "azurerm_virtual_network" "coop_vnet" {
+resource "azurerm_virtual_network" "vnet" {
   name                = var.vnet_name
   address_space       = var.address_space
   location            = azurerm_resource_group.resource_group.location
   resource_group_name = azurerm_resource_group.resource_group.name
 }
 
-resource "azurerm_subnet" "coop_subnets" {
+resource "azurerm_subnet" "subnets" {
   for_each             = var.subnets
   name                 = each.key
   resource_group_name  = azurerm_resource_group.resource_group.name
-  virtual_network_name = azurerm_virtual_network.coop_vnet.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = [each.value]
 }
 
@@ -30,7 +30,7 @@ resource "azurerm_route_table" "route_table" {
 }
 
 resource "azurerm_subnet_route_table_association" "subnet_route_table_associations" {
-  for_each       = azurerm_subnet.coop_subnets
+  for_each       = azurerm_subnet.subnets
   subnet_id      = each.value.id
   route_table_id = azurerm_route_table.route_table.id
 }
