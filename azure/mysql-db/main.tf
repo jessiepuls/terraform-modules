74bd55c53dd0a992300f54300443932cf3baf17c
Jessie Puls <jessiepuls@gmail.com>
2021-02-24 13:18:22 -0600

    Adding database

--- azure/mysql-db/main.tf
+++ azure/mysql-db/main.tf
@@ -18,10 +18,10 @@ resource "azurerm_mysql_server" "mysql_server" {
   storage_mb                       = 5120
 }
 
-# resource "azurerm_mysql_database" "example" {
-#   name                = "exampledb"
-#   resource_group_name = azurerm_resource_group.example.name
-#   server_name         = azurerm_mysql_server.example.name
-#   charset             = "utf8"
-#   collation           = "utf8_unicode_ci"
-# }
+resource "azurerm_mysql_database" "mysql_database" {
+  name                = var.db_name
+  resource_group_name = azurerm_resource_group.db_resource_group.name
+  server_name         = azurerm_mysql_server.mysql_server.name
+  charset             = "utf8"
+  collation           = "utf8_unicode_ci"
+}
