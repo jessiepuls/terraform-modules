a58ceff2bedb4ba39f2c28c1fd6a50630d1a41b9
Jessie Puls <jessiepuls@gmail.com>
2021-02-24 13:03:40 -0600

    Renaming mysql server resource and removing an accidentally left in access policy block

--- azure/mysql-db/key_vault.tf
+++ azure/mysql-db/key_vault.tf
@@ -29,23 +29,6 @@ resource "azurerm_key_vault" "mysql_key_vault" {
       "get",
     ]
   }
-
-  access_policy {
-    tenant_id = data.azurerm_client_config.current.tenant_id
-    object_id = data.azurerm_client_config.current.object_id
-
-    key_permissions = [
-      "get",
-    ]
-
-    secret_permissions = [
-      "get",
-    ]
-
-    storage_permissions = [
-      "get",
-    ]
-  }
 }
 
 resource "random_password" "mysql_admin_password" {
--- azure/mysql-db/main.tf
+++ azure/mysql-db/main.tf
@@ -3,7 +3,7 @@ resource "azurerm_resource_group" "db_resource_group" {
   location = var.location
 }
 
-resource "azurerm_mysql_server" "db" {
+resource "azurerm_mysql_server" "mysql_server" {
   name                = var.db_name
   location            = azurerm_resource_group.db_resource_group.location
   resource_group_name = azurerm_resource_group.db_resource_group.name
@@ -17,3 +17,11 @@ resource "azurerm_mysql_server" "db" {
   ssl_minimal_tls_version_enforced = "TLS1_2"
   storage_mb                       = 5120
 }
+
+# resource "azurerm_mysql_database" "example" {
+#   name                = "exampledb"
+#   resource_group_name = azurerm_resource_group.example.name
+#   server_name         = azurerm_mysql_server.example.name
+#   charset             = "utf8"
+#   collation           = "utf8_unicode_ci"
+# }
